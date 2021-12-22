bce13d78a8d84adf4935cf21ca1ad7d73d95fad7
Derek.Thomas2 <Derek.Thomas2@target.com>
2021-12-21 18:26:25 -0800

    updating api call location

--- src/EventService.js
+++ src/EventService.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 export default {
   async getLog4jData() {
-    let res = await axios.get('https://raw.githubusercontent.com/cisagov/log4j-affected-db/develop/README.md')
+    let res = await axios.get('https://raw.githubusercontent.com/cisagov/log4j-affected-db/develop/SOFTWARE-LIST.md')
     return res.data
   }
 }
